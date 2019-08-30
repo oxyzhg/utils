2a8bb35b6b6fc39b7e8e9816db738c3f41f0769c
kq <oxyzhg@163.com>
2019-08-30 23:50:45 +0800

    add type detect function

---
+++ type-detect.js
@@ -0,0 +1,54 @@
+function isNumber(value) {
+  return (
+    typeof value == 'number' ||
+    Object.prototype.toString.call(value).slice(8, -1) == 'Number'
+  );
+}
+
+function isString(value) {
+  return (
+    typeof value == 'string' ||
+    Object.prototype.toString.call(value).slice(8, -1) == 'String'
+  );
+}
+
+function isBoolean(value) {
+  return (
+    typeof value == 'boolean' ||
+    Object.prototype.toString.call(value).slice(8, -1) == 'Boolean'
+  );
+}
+
+function isUndefined(value) {
+  return value === undefined;
+}
+
+function isNull(value) {
+  return value === null;
+}
+
+function isArray(value) {
+  return Array.isArray(value);
+}
+
+function isArrayLike(value) {
+  return value != null && isLength(value.length) && isFunction(value);
+}
+
+function isObject(value) {
+  return (
+    value != null && (typeof value == 'object' || typeof value == 'function')
+  );
+}
+function isObjectLike(value) {
+  return value != null && typeof value == 'object';
+}
+function isFunction(value) {
+  return Object.prototype.toString.call(value).slice(8, -1) == 'Function';
+}
+function isRegExp(value) {}
+function isDate(value) {}
+function isBuffer(value) {}
+function isSymbol(value) {}
+function isSet(value) {}
+function isMap(value) {}
