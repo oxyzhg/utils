553aaf18fb2f744b95a2095565b5d7932d4fd688
kq <oxyzhg@163.com>
2019-09-08 00:13:09 +0800

    reversion type detect

--- type-detect.js
+++ type-detect.js
@@ -1,22 +1,36 @@
+const MAX_SAFE_INTEGER = 9007199254740991;
+const MAX_ARRAY_LENGTH = 4294967295;
+
+const numberTag = '[object Number]';
+const stringTag = '[object String]';
+const boolTag = '[object Boolean]';
+const arrayTag = '[object Array]';
+const objectTag = '[object Object]';
+const funcTag = '[object Function]';
+const argsTag = '[object Arguments]';
+const dateTag = '[object Date]';
+const regexpTag = '[object RegExp]';
+const errorTag = '[object Error]';
+const symbolTag = '[object Symbol]';
+const setTag = '[object Set]';
+const mapTag = '[object Map]';
+const weakSetTag = '[object WeakSet]';
+const weakMapTag = '[object WeakMap]';
+const promiseTag = '[object Promise]';
+const genTag = '[object GeneratorFunction]';
+
+const getType = target => Object.prototype.toString.call(target);
+
 function isNumber(value) {
-  return (
-    typeof value == 'number' ||
-    Object.prototype.toString.call(value).slice(8, -1) == 'Number'
-  );
+  return typeof value == 'number' || getType(value) === numberTag;
 }
 
 function isString(value) {
-  return (
-    typeof value == 'string' ||
-    Object.prototype.toString.call(value).slice(8, -1) == 'String'
-  );
+  return typeof value == 'string' || getType(value) === stringTag;
 }
 
 function isBoolean(value) {
-  return (
-    typeof value == 'boolean' ||
-    Object.prototype.toString.call(value).slice(8, -1) == 'Boolean'
-  );
+  return typeof value == 'boolean' || getType(value) === boolTag;
 }
 
 function isUndefined(value) {
@@ -32,35 +46,57 @@ function isArray(value) {
 }
 
 function isArrayLike(value) {
-  return value != null && isLength(value.length) && isFunction(value);
+  return value != null && isLength(value.length) && !isFunction(value);
 }
 
 function isObject(value) {
-  return (
-    value != null && (typeof value == 'object' || typeof value == 'function')
-  );
+  return value != null && (typeof value == 'object' || typeof value == 'function');
 }
 
 function isObjectLike(value) {
   return value != null && typeof value == 'object';
 }
 
-function isFunction(value) {
-  return Object.prototype.toString.call(value).slice(8, -1) == 'Function';
+function isArrayLikeObject(value) {
+  return isObjectLike(value) && isArrayLike(value);
 }
 
-function isRegExp(value) {}
-function isDate(value) {}
-function isBuffer(value) {}
-function isSymbol(value) {}
+function isFunction(value) {
+  return getType(value) == funcTag || getType(value) == genTag;
+}
+
+function isArguments(value) {
+  return (
+    isArrayLikeObject(value) &&
+    Object.prototype.hasOwnProperty.call(value, 'callee') &&
+    (Object.prototype.propertyIsEnumerable.call(value, 'callee') || getType(value) == argsTag)
+  );
+}
+
+function isSymbol(value) {
+  return typeof value == 'symbol' || getType(value) === symbolTag;
+}
+
 function isSet(value) {}
 function isMap(value) {}
+function isWeakSet(value) {}
+function isWeakMap(value) {}
 
 function isLength(value) {
-  return (
-    typeof value == 'number' &&
-    value > -1 &&
-    value % 1 == 0 &&
-    value < 9007199254740991
-  );
+  return typeof value == 'number' && value >= 0 && value % 1 == 0 && value < MAX_ARRAY_LENGTH;
+}
+
+function isEmpty(value) {
+  if (isArrayLike(value) && (isArray(value) || isString(value) || isArguments(value))) {
+    return !value.length;
+  }
+  const tag = getType(value);
+  if (tag == setTag || tag == mapTag) {
+    return !value.size;
+  }
+  // isPrototype
+  for (let key in value) {
+    if (Object.prototype.hasOwnProperty.call(value, key)) return false;
+  }
+  return true;
 }
